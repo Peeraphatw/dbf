7ff6c8ae84510814d65993a4c85f45c7fe1e0666
Peeraphatw <75972122+Peeraphatw@users.noreply.github.com>
2021-01-26 21:52:11 +0700

    api

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const { DBFFile } = require('dbffile');
-var output = [];
-async function readFileDBF() {
-  let dbf = await DBFFile.open('DBF/ARMAS.DBF', { encoding: 'ISO-8859-11' });
+const { DBFFile } = require("dbffile");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+var output;
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  const readFileDBF = async () => {
+    let dbf = await DBFFile.open("DBF/ARMAS.DBF", { encoding: "ISO-8859-11" });
 
-  console.log(`DBF file contains ${dbf.recordCount} records.`);
-  console.log(`Field names: ${dbf.fields.map((f) => f.name).join(', ')}`);
-  let records = await dbf.readRecords(2);
+    //console.log(`DBF file contains ${dbf.recordCount} records.`);
+    //console.log(`Field names: ${dbf.fields.map((f) => f.name).join(", ")}`);
+    let records = await dbf.readRecords(dbf.recordCount);
+    //console.log(records);
+    return records;
+  };
+  readFileDBF().then((data) => res.status(200).json({ data: data }));
+});
 
-  for (let record of records) {
-    console.log(record);
-  }
-}
-
-readFileDBF();
-console.log(output);
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
